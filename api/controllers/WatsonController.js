5dcf482fd05b44a7e21de972b39b6c733c0e9fe6
Scott McAllister <scottm@nextopia.com>
2016-03-01 17:06:51 -0500

    added npm module for visual recognition to controller

--- api/controllers/WatsonController.js
+++ api/controllers/WatsonController.js
@@ -5,24 +5,48 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+var watson = require('watson-developer-cloud');
+var fs = require('fs');
+
+var visual_recognition = watson.visual_recognition({
+	username: '91b91e0c-9e3e-496f-8be8-8d99257112e4',
+	password: 'oGm2l96oVZl4',
+	version: 'v2-beta',
+	version_date: '2015-12-02'
+});
+
 module.exports = {
 
 	uploadImage: function(req, res){
-		req.file('image').upload({
-			// don't allow the total upload size to exceed ~20MB
-			maxBytes: 20000000
-			},function whenDone(err, uploadedFiles) {
-			if (err) {
-				return res.negotiate(err);
-			}
+		req.file('files').upload({
+				// don't allow the total upload size to exceed ~20MB
+				maxBytes: 20000000
+			},
+			function whenDone(err, uploadedFiles) {
+				if (err) {
+					return res.negotiate(err);
+				}
 
-			// If no files were uploaded, respond with an error.
-			if (uploadedFiles.length === 0){
-				return res.badRequest('No file was uploaded');
-			}
+				// If no files were uploaded, respond with an error.
+				if (uploadedFiles.length === 0){
+					return res.badRequest('No file was uploaded');
+				}
 
-			return res.ok();
-		});
+				console.log(uploadedFiles);
+
+				var params = {
+					images_file: fs.createReadStream(uploadedFiles)
+				};
+
+				visual_recognition.classify(params, function(err, res){
+					if(err)
+						return res.negotiate(err);
+					else
+						return res.json(res);
+				});
+
+				return res.ok();
+			});
 	}
 	
 };
