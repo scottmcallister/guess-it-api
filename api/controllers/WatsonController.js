2c6f081177c86430471d368342db3d7468023c39
Scott McAllister <scottm@nextopia.com>
2016-02-29 22:25:29 -0500

    chnaged name of image field in request to 'image'

--- api/controllers/WatsonController.js
+++ api/controllers/WatsonController.js
@@ -8,7 +8,7 @@
 module.exports = {
 
 	uploadImage: function(req, res){
-		req.file('files').upload({
+		req.file('image').upload({
 			// don't allow the total upload size to exceed ~20MB
 			maxBytes: 20000000
 			},function whenDone(err, uploadedFiles) {
